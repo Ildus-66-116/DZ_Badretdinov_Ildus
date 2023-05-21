74bba11f85b88824403839b462a65b627170d4d9
ILDUS <ildus6696@gmail.com>
2023-05-21 20:31:51 +0300

    Решена задача 2

---
+++ Задача 2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите 1 число ");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 2 число ");
+int m = int.Parse(Console.ReadLine()!);
+if(n > m)
+    Console.Write($"max = {n} min = {m}");
+    else 
+    Console.Write($"max = {m} min = {n}");
