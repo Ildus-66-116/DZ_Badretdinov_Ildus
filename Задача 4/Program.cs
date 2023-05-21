e1a9e26e2c1b6010362ebaf8e6f9458f304961e1
ILDUS <ildus6696@gmail.com>
2023-05-21 21:05:25 +0300

    Решена задача 4

---
+++ Задача 4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите 1 число ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 2 число ");
+int b = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 3 число ");
+int c = int.Parse(Console.ReadLine()!);
+if(a > b && a > c)
+Console.Write ($"max = {a}");
+if(b > a && b > c)
+Console.Write ($"max = {b}");
+if(c > a && c > b)
+Console.Write ($"max = {c}");
